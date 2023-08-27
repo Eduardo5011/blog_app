dc7cac01571e76a3d79bec6fb3314a51b388b5ca
Eduardo5011 <eduardo.rosado5011@gmail.com>
2023-08-27 18:08:49 -0400

    updated

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import axios from "../utils/axios";
+import { Navigate } from "react-router-dom";
 
 const LOGIN_URL = "/login";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,15 +19,26 @@ const Login = () => {
         {
           headers: { "Content-Type": "application/json" },
           withCredentials: true,
+          credentials: "include",
         }
       );
+      if (response.status === 200) {
+        setRedirect(true);
+      } else {
+        alert("wrong credentials");
+      }
       console.log(response);
-      setUsername("")
-      setPassword("")
+      setUsername("");
+      setPassword("");
     } catch (err) {
       console.log(err);
+      alert("wrong credentials");
     }
   };
+
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
 
   return (
     <form className="login" onSubmit={handleSubmit}>
